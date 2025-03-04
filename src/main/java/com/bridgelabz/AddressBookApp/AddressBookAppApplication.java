8c43d2be1acb0b6b3ca9a966cb6df41e043679f8
niketajain123 <niketajain236@gmail.com>
2025-03-04 11:36:18 +0530

    [Niketa Jain]Add.UC8: Added logging levels, logging to file in production, and profile-based configurations.

--- src/main/java/com/bridgelabz/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/AddressBookApp/AddressBookAppApplication.java
@@ -1,16 +1,17 @@
 package com.bridgelabz.AddressBookApp;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class AddressBookAppApplication {
+	private static final Logger logger = LoggerFactory.getLogger(AddressBookAppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
-		log.info("Application is started...");
+		logger.info("Application is started...");
 
 	}
 
