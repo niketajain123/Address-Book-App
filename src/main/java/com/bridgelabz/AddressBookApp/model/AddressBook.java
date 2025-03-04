eb0d0d17613fb300176b605ac2cd730e00450793
niketajain123 <niketajain236@gmail.com>
2025-03-04 11:08:30 +0530

    [Niketa Jain]Add.UC6: Added lombok integration for DTO and Model.

--- src/main/java/com/bridgelabz/AddressBookApp/model/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBookApp/model/AddressBook.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @JsonPropertyOrder({"id", "name", "phone", "email"})
 public class AddressBook {
 
@@ -18,40 +22,9 @@ public class AddressBook {
     private String email;
 
     public AddressBook(String name, String phone, String email) {
+        this.id=id;
         this.name = name;
         this.phone = phone;
         this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
